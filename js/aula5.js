aa3fa1fd60e75f9ebfb1a01900f27cec80de43c1
lincolnfrias <lincolnfrias@gmail.com>
2018-10-05 09:43:09 -0300

    .

--- js/aula5.js
+++ js/aula5.js
@@ -2,27 +2,75 @@
 let lista1 = [341.1, 5.2, 67.0];
 let maximo = math.max(lista1);
 console.log(`o valor máximo é ${maximo}.`);
+$('#sp1').html(maximo);
 
-// média e mediana
+// média, mediana, somatório
 let media = math.mean(lista1);
 console.log(`o valor médio é ${math.round(media, 2)}.`);
+$('#sp2').html(math.round(media, 2));
+
 
 // desvio padrão e variância (var)
 let dp = math.std(lista1);
 console.log(`o desvio padrão é ${math.round(dp, 2)}.`);
 
-function soma(a, b) {
-    return a + b
-};
+// range
+let lista2 = math.range(0, 4);
+console.log(`a lista criada é ${lista2._data}`);
+
+// encadeando operações
+let resultadoEncadeado = math.chain(3.66789).round(2).add(5).multiply(20);
+console.log(`o resultado da operação encadeada foi ${resultadoEncadeado}`);
+
+// potencia, raiz quadrada, log, log10, e, pi
+let potencia = math.pow(2, 8);
+let raiz = math.sqrt(9);
+let logaritmoNatural = math.log(2.72);
+let logaritmoBaseDez = math.log10(100);
+
+objeto1 = {'potencia': potencia,
+           'raiz': raiz,
+           'logaritmoNatural': math.round(logaritmoNatural, 4),
+           'logaritmoBaseDez': logaritmoBaseDez,
+           'e': math.round(math.e, 4),
+           'pi': math.round(math.pi, 4)};
+
+console.log(objeto1);
+
+// operações vetoriais
+let vetor1 = [20, 15, 4];
+let vetor2 = [109, 3, 44];
+let resultado1 = math.add(2, vetor1);
+let resultado2 = math.add(vetor1, vetor2);
+let resultado3 = math.multiply(2, vetor1);
+let resultado4 = math.dotMultiply(vetor1, vetor2);         // element-wise
+let resultado5 = math.dotMultiply(resultado1, resultado2);
+
+objeto2 = {'resultado1': resultado1,
+           'resultado2': resultado2,
+           'resultado3': resultado3,
+           'resultado4': resultado4,
+           'resultado5': resultado5};
+
+console.log(objeto2);
+
+// operações matriciais
+let X1 = math.matrix([[0, 1], [2, 3], [4, 5]]);
+let X2 = math.add(3, X1);
+let X3 = math.dotMultiply(X1, X2);   // element-wise
+let X2T = math.transpose(X2)
+let X4 = math.multiply(X1, X2T);    // multiplicação matricial
+console.log(X2._data);
+console.log(`o tamanho da matriz é ${X2._size[0]} por ${X2._size[1]}.`);
+console.log(X3._data)
+console.log(X4._data)
 
 
 
-let a = ss.linearRegression([[0, 0], [1, 1]]);
-$('#sp1').text(a['m']);
 
-let b = math.chain(3.66789).round(2).add(5).multiply(20);
-$("#sp4").text(b);
+// resize, transpose, subset, diag, identity, det,  ones, zeros
+// mathjs.org/docs/datatypes/matrices.html
 
-
-let dados = [3, 4, 5];
-
+// álgebra e derivadas
+// math.simplify('x^2 + x + 3 + x^2').toString())
+// math.derivative('sin(2x)', 'x').toString()
--- js/aula6.js
+++ js/aula6.js
@@ -1,80 +1,87 @@
-maximo = dl.max([1000, 2, 2000, 3]); // Math.max(...array)
-$('#um').html(maximo);
+// ler csv
+let teste = dl.csv('dados/teste.csv');
+let stocks = dl.csv('dados/stocks.csv');
+
+// head(), mostrar os dados como tabela
+console.log(dl.format.table(teste, {limit: 2}));
+
+// mostrar colunas
+let colunas = teste.columns.toString()
+$('#um').html(`As colunas do conjunto de dados são ${colunas}.`);
+
+// .toString() é necessário para um valor poder ser mostrado na página,
+// ao invés de ser apenas utilizado em cálculos.
+
+// resumo estatístico
+console.log(dl.format.summary(teste));
+
+// selecionar apenas uma coluna
+let preco = teste.map(dl.$('preco'));
+
+// resumo estatístico de apenas uma coluna
+console.log(dl.profile(preco));
+
+// histograma
+console.log(dl.histogram(teste, 'preco', {maxbins: 2}))
+
+// tabela de frequência: value_counts
+console.log(dl.count.map(teste.map(dl.$('cat'))));
+
+// subselecionar o array: de 2 até 5
+console.log(`subseleção: ${preco.slice(2, 5)}`);
+
+// quantil
+console.log(`A quantil solicitado é ${dl.quantile(preco, 0.75)}`);
+
+// ordenar (sort)
+// sinal de menos indica ordem decrescente
+console.log('valores ordenados', teste.sort(dl.comparator('-preco'))) 
+
+// filtrar dados, query
+console.log('valores filtrados', teste.filter(function (d) { return d.preco > 100 }));
+
+
+// amplitude
+let amplitude = dl.extent(teste, 'preco');
+console.log(`A amplitude do preço é ${amplitude}.`);
+
+// unique
+console.log('valores únicos', dl.unique(teste, 'preco'));
+
+// groupby
+console.log('saída do groupby', dl.groupby('cat').execute(teste));
+
+let resumo = dl.groupby('cat').summarize({ 'preco': 'sum' }).execute(teste)
+console.log(dl.print.table(resumo))
+
+// correlação
+let correlacao = dl.cor(teste, 'id', 'preco');
+console.log(`A correlação entre data e preço é ${math.round(correlacao, 2)}`);
+
+// regressão linear
+console.log('saída da regressão', dl.linearRegression(teste, 'preco', 'id'));
 
 
 
-// d3.csv('data/cities.csv').then(function (data) {
-//     data.forEach(function (d) {
-//         d.population = +d.population;
-//         d["land area"] = +d["land area"];
-//     });
-//     console.log(data);
-// });
 
 
+let fields = [
+    "CMTE_ID",              // committee id
+    "CMTE_NM",              // committee name
+    "TRES_NM",              // treasurer name
+    "CMTE_ST1",             // street address line 1
+    "CMTE_ST2",             // street address line 2
+    "CMTE_CITY",            // city
+    "CMTE_ST",              // state
+    "CMTE_ZIP",             // zip code
+    "CMTE_DSGN",            // designation
+    "CMTE_TP",              // type
+    "CMTE_PTY_AFFILIATION", // party affiliation
+    "CMTE_FILING_FREQ",     // filing frequency
+    "ORG_TP",               // interest group category
+    "CONNECTED_ORG_NM",     // connected organization name (if any)
+    "CAND_ID"               // candidate id (if any)
+];
 
-var coisa = dl.csv('https://vega.github.io/datalib/data/stocks.csv');
+let cm = dl.dsv('dados/cm.txt', { delimiter: '|', header: fields });
 
-// Show summary statistics for each column of the data table.
-console.log(dl.format.summary(coisa));
-
-// json > csv: não precisa de conversão 
-d3.json("/data/employees.json").then(function (data) {
-    console.log(data[0]);
-});
-
-// merging data
-const articles = [{
-    "id": 1,
-    "name": "vacuum cleaner",
-    "weight": 9.9,
-    "price": 89.9,
-    "brand_id": 2
-}, {
-    "id": 2,
-    "name": "washing machine",
-    "weight": 540,
-    "price": 230,
-    "brand_id": 1
-}, {
-    "id": 3,
-    "name": "hair dryer",
-    "weight": 1.2,
-    "price": 24.99,
-    "brand_id": 2
-}, {
-    "id": 4,
-    "name": "super fast laptop",
-    "weight": 400,
-    "price": 899.9,
-    "brand_id": 3
-}];
-
-const brands = [{
-    "id": 1,
-    "name": "SuperKitchen"
-}, {
-    "id": 2,
-    "name": "HomeSweetHome"
-}];
-
-let formatacao = function (row) {
-    return {
-        name: row.name,
-        age: +row.age,
-        color: row.color
-    };
-}
-
-let callback = function (data) {
-
-    var averageAge = data.reduce(function (prev, curr) {
-        return prev + curr.age;
-    }, 0) / data.length;
-
-    var ulSelection = d3.select('#lista');
-    var valuesSelection = ulSelection.selectAll('li').data(data).enter().append('li').text(function (d) { return d.name; });
-    var totalSelection = ulSelection.append('li').text('Average: ' + averageAge);
-};
-
-d3.csv('dados/personagens.csv', formatacao).then(callback);
